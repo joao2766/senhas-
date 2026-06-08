550b575169a0add24c938d315a265fcc38abe7bf
joao2766 <joao.siqueira27@escola.pr.gov.br>
2026-06-08 08:13:11 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+const numeroSenha = document.querySelector('.parametro-senha__texto');   
+
+ numeroSenha.textContent = 5;
+
+const botoes = document.querySelectorAll('.parametro-senha__botao');
+
+botoes[0].onclik
+console.log(botoes);
